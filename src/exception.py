cb4c920951071ed156c3d8cae59d876257c1fee8
Palak Nagar <palaknagar13@gmail.com>
2026-04-12 16:03:27 +0530

    Implemented customized extension

--- src/exception.py
+++ src/exception.py
@@ -1,4 +1,6 @@
 import sys
+import logging
+import logger
 
 def error_message_detail(error, error_detail:sys):
 
@@ -17,10 +19,19 @@ def error_message_detail(error, error_detail:sys):
 
 class CustomException(Exception):
     def __init__(self, error_message, error_details:sys):
-        super.__init__(error_message)
+        super().__init__(error_message)
         self.error_message = error_message_detail(error_message, error_detail=error_details)
 
 
     def __str__(self):
         return self.error_message
+    
+
+if __name__ == "__main__":   
+    try:
+        a = 1/0
+    except Exception as e:
+        logging.error("Division by zero")
+        raise CustomException(e,sys)    
+
 
